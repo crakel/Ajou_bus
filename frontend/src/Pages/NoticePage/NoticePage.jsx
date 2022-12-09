3f1d591eb44cbc982696f3348543644dd18599fd
jin-Pro <dnjun2@ajou.ac.kr>
2022-12-09 17:30:27 +0900

    enhance: NoticePage api

--- frontend/src/Pages/NoticePage/NoticePage.jsx
+++ frontend/src/Pages/NoticePage/NoticePage.jsx
@@ -11,11 +11,11 @@ export const NoticePage = () => {
       <div id="head_outer_div">
         <div id="main_head_frame">
           <button id="head_menu_button">
-            <img alt="menu_button" src="assets/img/menubuttonimage.png" />
+            <img alt="menu_button" src="/assets/img/menubuttonimage.png" />
           </button>
           <h1>공지사항</h1>
           <button id="head_login_button">
-            <img alt="login_button" src="assets/img/user.png" />
+            <img alt="login_button" src="/assets/img/user.png" />
           </button>
         </div>
       </div>
